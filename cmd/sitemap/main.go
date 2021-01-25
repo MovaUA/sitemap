af9249e4f0239c2d8254bee77af6a6416ca79209
mova <valeriy.molchanov.77@gmail.com>
2021-01-25 14:34:18 +0200

    cmd/sitemap

--- cmd/sitemap/main.go
+++ cmd/sitemap/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"encoding/xml"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/movaua/sitemap/pkg/sitemap"
+)
 
 func main() {
-	fmt.Println("sitemap")
+	url := flag.String("url", "", "[required] an URL to the site")
+	flag.Parse()
+	if *url == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	urlset, err := sitemap.Build(*url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	out, err := xml.MarshalIndent(urlset, "", "  ")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("%s%s\n", xml.Header, out)
 }
--- pkg/sitemap/sitemap.go
+++ pkg/sitemap/sitemap.go
@@ -1,5 +1,29 @@
 package sitemap
 
 import (
+	"encoding/xml"
+
 	_ "github.com/movaua/link/pkg/link"
 )
+
+// URLSet is a top-level model for sitemap.
+// It encapsulates the file and references the current protocol standard.
+// See https://www.sitemaps.org/protocol.html
+type URLSet struct {
+	XMLName xml.Name   `xml:"http://www.sitemaps.org/schemas/sitemap/0.9 urlset"`
+	Entries []URLEntry `xml:"url"`
+}
+
+// URLEntry is a parent tag for each URL entry
+type URLEntry struct {
+	URL string `xml:"loc"`
+	Err error  `xml:"-"`
+}
+
+// Build walks all the links on the site provided by url
+// and returns sitemap with links belonging to the same domain
+// as of the provided url, or an error
+func Build(url string) (URLSet, error) {
+	// TODO: implement building of URLSet
+	return URLSet{}, nil
+}
