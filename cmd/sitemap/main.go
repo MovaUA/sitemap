242a47bb15986f5d26ace01c2a8e511e5b22d26e
mova <valeriy.molchanov.77@gmail.com>
2021-01-27 18:02:38 +0200

    defaults

--- cmd/sitemap/main.go
+++ cmd/sitemap/main.go
@@ -15,7 +15,7 @@ import (
 func main() {
 	urlFlag := flag.String("url", "https://github.com/movaua/sitemap", "the URL of the site you want to build a sitemap for")
 	maxDepth := flag.Int("depth", 3, "a maximum depth of pages to traverse")
-	timeout := flag.Int("timeout", 3, "timeout in seconds to wait for response from a single HTTP request")
+	timeout := flag.Int("timeout", 5, "timeout in seconds to wait for response from a single HTTP request")
 	requests := flag.Int("requests", 0, "max concurrent HTTP requests. 0 means number of CPUs.")
 	flag.Parse()
 
